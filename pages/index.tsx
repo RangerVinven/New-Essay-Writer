27c02372b062b4cb58e84099648db52d227e7a9a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-10 23:34:15 +0000

    Added a call to api/GetEssays

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import EssayTile from "../components/HomePage/EssayTile";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+	const [essays, setEssays] = useState(["Why Edward Snowden Should Not Be Extradited", "The Consequences of Cyber War", "TypeScript vs JavaScript", "The Difference Between Penetration Testing and Bug Bounty", "Is There A Best Programming Language?", "How To Learn To Programming"]);
+	
+	// Gets the user's essays
+	useEffect(() => {
+		fetch("http://localhost:3000/api/GetEssays").then(data => data.json()).then(essayNames => {
+			setEssays(essayNames.EssayNames);			
+		});
+	}, []);
+
 	return (
 		<div className="w-full h-screen flex justify-center items-center">
 			<div className="flex flex-wrap justify-evenly w-2/3">
-				<EssayTile name="Why Edward Snowden Should Be Extradited" />
-				<EssayTile name="The Consequences of Cyber War" />
-				<EssayTile name="TypeScript vs JavaScript" />
-				<EssayTile name="The Difference Between Penetration Testing and Bug Bounty" />
-				<EssayTile name="Is There A Best Programming Language?" />
-				<EssayTile name="How To Learn To Programming" />
+				{
+					essays.map(essay => {
+						return <EssayTile name={essay} />
+					})
+				}
 			</div>
 		</div>
 	)
