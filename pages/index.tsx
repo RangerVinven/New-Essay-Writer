7c9b7baed8b46587e5326dc2103c1230d3c0d2f5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-10 22:50:52 +0000

    Added EssayTile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import EssayTile from "../components/HomePage/EssayTile";
+
 export default function Home() {
 	return (
-		<div>
-			
+		<div className="w-full h-screen flex justify-center items-center">
+			<div className="flex flex-wrap justify-evenly w-2/3">
+				<EssayTile name="Why Edward Snowden Should Be Extradited" />
+				<EssayTile name="The Consequences of Cyber War" />
+				<EssayTile name="TypeScript vs JavaScript" />
+				<EssayTile name="The Difference Between Penetration Testing and Bug Bounty" />
+				<EssayTile name="Is There A Best Programming Language?" />
+				<EssayTile name="How To Learn To Programming" />
+			</div>
 		</div>
 	)
 }
