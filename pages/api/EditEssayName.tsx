f2a5e300fdcb9de3f1d9769d42835c8392958c32
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-11 13:42:21 +0000

    Added EditEssayName endpoint

---
+++ pages/api/EditEssayName.tsx
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "../../lib/PrismaClient";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+    // Rejects the request if there's no old and new essay name
+    if(!req.body.id || !req.body.NewEssayName) res.status(400).json({ message: "Invalid request" });
+
+	prisma.essays.update({
+        where: {
+            id: req.body.id
+        },
+        data: {
+            Name: req.body.NewEssayName
+        }
+    }).then(() => {
+        res.status(200).json({});
+    }).catch(() => {
+        res.status(500).json({});
+    });
+}
