72bcbb8b4f9b1fcba56c91ad33c2cedde6153c64
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-11 00:47:41 +0000

    Increased the padding to 2

--- components/HomePage/EssayTile.tsx
+++ components/HomePage/EssayTile.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function EssayTile(props: Props) {
     return (
-        <div className="h-64 w-64 mb-11 rounded-md bg-gray-400 flex justify-center items-center p-1 hover:cursor-pointer">
+        <div className="h-64 w-64 mb-11 rounded-md bg-gray-400 flex justify-center items-center p-2 hover:cursor-pointer">
             <h3 className="text-center text-xl">{props.name}</h3>
         </div>
     )
