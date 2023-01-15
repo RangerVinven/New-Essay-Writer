10c4ae9085d14de56730d2e1595e966cbdfa0632
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-15 12:10:21 +0000

    Added loading animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,49 @@ import Link from 'next/link'
 import { useDisclosure } from "@chakra-ui/react";
 import AddEssayModal from "../components/HomePage/AddEssayModal";
 import { Essay } from "../lib/Types";
+import LoadingSpinner from "../components/HomePage/General/LoadingSpinner";
 
 export default function Home() {
 
 	const [essays, setEssays] = useState<Essay[]>([]);
-
+	const [loading, setLoading] = useState(true);
     const { isOpen, onOpen, onClose } = useDisclosure() // For the add essay modal
 
 	// Gets the user's essays
 	useEffect(() => {
+		setLoading(true); // Starts the loading animation
+
 		fetch("http://localhost:3000/api/GetEssays").then(data => data.json()).then(essayNames => {
+			
+			setLoading(false); // Starts the loading animation
 			setEssays(essayNames.EssayNames);
+
 		});
 	}, []);
 
 	return (
-		<div className="w-full h-fit my-16 flex justify-center items-center">
-			<AddEssayModal essays={essays} setEssays={setEssays} isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+		<div className="flex justify-center items-center h-screen">
 
-			<div className="flex flex-wrap justify-evenly w-2/3">
-				{
-					essays.map((essay) => {
-						return <EssayTile essays={essays} setEssays={setEssays} key={essay.id} id={essay.id} name={essay.Name} />
-					})
-				}
+			{
+				loading ? <LoadingSpinner /> : <div className="relative w-full h-fit my-16 flex justify-center items-center">
+					
+					<AddEssayModal essays={essays} setEssays={setEssays} isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
 
-				<div onClick={onOpen} className="flex text-center justify-center items-center hover:cursor-pointer w-64 h-64 border-gray-400 border-dashed border-2">
-					<h3 className="text-gray-400 text-6xl font-thin">+</h3>
+					<div className="flex flex-wrap justify-evenly w-2/3">
+						{
+							essays.map((essay) => {
+								return <EssayTile essays={essays} setEssays={setEssays} key={essay.id} id={essay.id} name={essay.Name} />
+							})
+						}
+
+						<div onClick={onOpen} className="flex text-center justify-center items-center hover:cursor-pointer w-64 h-64 border-gray-400 border-dashed border-2">
+							<h3 className="text-gray-400 text-6xl font-thin">+</h3>
+						</div>
+
+					</div>
+
 				</div>
-
-			</div>
+			}
 		</div>
 	)
 }
