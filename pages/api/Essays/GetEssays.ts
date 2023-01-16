35c7b621254fa329c41f2377d2bb22f98e49fdaf
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-16 18:40:51 +0000

    Updated PrismaClient import

--- pages/api/Essays/GetEssays.ts
+++ pages/api/Essays/GetEssays.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
-import { prisma } from "../../lib/PrismaClient";
+import { prisma } from "../../../lib/PrismaClient";
 
 type Data = {
   	EssayNames: Object
