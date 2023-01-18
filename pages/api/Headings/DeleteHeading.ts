27501ec45b140c5099b58760c8ceecf316e127ec
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-18 23:47:20 +0000

    Created DeleteHeading endpoint

---
+++ pages/api/Headings/DeleteHeading.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "../../../lib/PrismaClient";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+    if(!req.body.id) res.status(400).json({message: "Invalid request"});
+
+	prisma.headers.delete({
+        where: {
+            id: req.body.id
+        }
+    }).then(() => {
+        res.status(200).json({});
+    }).catch(() => {
+        res.status(500).json({});
+    })
+}
