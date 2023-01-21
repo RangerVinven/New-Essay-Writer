baa7a2c26b5ea04043fed70ac7e436eebcc4ad96
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-01-21 01:09:58 +0000

    Added Edit headings option

--- components/EssayEdit/Heading.tsx
+++ components/EssayEdit/Heading.tsx
@@ -1,8 +1,9 @@
-import { DeleteIcon } from '@chakra-ui/icons'
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
 import { IconButton, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { Header } from '../../lib/Types'
 import DeleteHeadingModal from './DeleteHeadingsModal'
+import RenameHeadingModal from './RenameHeadingsModal'
 
 type Props = {
     header: Header
@@ -13,14 +14,17 @@ type Props = {
 
 export default function Heading(props: Props) {
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen: deleteIsOpen, onOpen: deleteOnOpen, onClose: deleteOnClose } = useDisclosure();
+    const { isOpen: editIsOpen, onOpen: editOnOpen, onClose: editOnClose } = useDisclosure();
 
     return (
         <div className="mb-2 text-center flex items-center justify-center">
-            <DeleteHeadingModal headerId={props.header.id} header={props.header.Name} headings={props.headings} setHeadings={props.setHeadings} isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+            <DeleteHeadingModal headerId={props.header.id} header={props.header.Name} headings={props.headings} setHeadings={props.setHeadings} isOpen={deleteIsOpen} onOpen={deleteOnOpen} onClose={deleteOnClose} />
+            <RenameHeadingModal headerId={props.header.id} header={props.header.Name} headings={props.headings} setHeadings={props.setHeadings} isOpen={editIsOpen} onOpen={editOnOpen} onClose={editOnClose} />
 
             <h3 className="text-xl font-medium mr-4">{ props.header.Name }</h3>
-            <IconButton size="sm" onClick={onOpen} aria-label="Delete Heading" icon={<DeleteIcon />} colorScheme="red"/>
+            <IconButton className="mr-2" size="sm" onClick={editOnOpen} aria-label="Edit Heading" icon={<EditIcon />} colorScheme="blue"/>
+            <IconButton size="sm" onClick={deleteOnOpen} aria-label="Delete Heading" icon={<DeleteIcon />} colorScheme="red"/>
         </div>
     )
 }
