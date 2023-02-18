8e686a0593b737f812be99215b18f0377ee4b970
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-02-18 21:08:27 +0000

    Added alternative sentences to the popover

--- components/EditSentences/SentencePopover.tsx
+++ components/EditSentences/SentencePopover.tsx
@@ -1,20 +1,43 @@
 import { AddIcon } from '@chakra-ui/icons';
-import { Input, Popover, PopoverTrigger, Button, PopoverContent, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody, InputGroup, InputRightElement, IconButton } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import { Sentence } from '../../lib/Types';
+import { Input, Popover, PopoverTrigger, Button, PopoverContent, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody, InputGroup, InputRightElement, IconButton, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { AlternativeSentence, Sentence } from '../../lib/Types';
+import LoadingSpinner from '../General/LoadingSpinner';
 
 type Props = {
     sentence: Sentence
+}
+
+async function getAlternativeSentences(sentenceId: number, setAlternativeSentencesLoading: Function, setAlternativeSentences: Function) {
+    fetch("http://localhost:3000/api/AlternativeSentence/GetAlternativeSentences", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            sentenceId: sentenceId
+        })
+    }).then(res => res.json()).then(res => {
+        setAlternativeSentencesLoading(false);
+        setAlternativeSentences(res.sentences);
+    })
 }
 
 export default function SentencePopover(props: Props) {
 
     const [newSentence, setNewSentence] = useState("");
 
+    const [selectedSentence, setSelectedSentence] = useState("1");
+    const [alternativeSentences, setAlternativeSentences] = useState<AlternativeSentence[]>([]);
+
+    const [alternativeSentencesLoading, setAlternativeSentencesLoading] = useState(false);
+
     return (
         <Popover placement='top' isLazy>
             <PopoverTrigger>
-                <h3 className="mb-10 hover:cursor-pointer">{props.sentence.Sentence}</h3>
+                <h3 onClick={() => {
+                    getAlternativeSentences(props.sentence.id, setAlternativeSentencesLoading, setAlternativeSentences);
+                }} className="mb-10 hover:cursor-pointer">{props.sentence.Sentence}</h3>
             </PopoverTrigger>
             <PopoverContent>
                 <PopoverHeader fontWeight='semibold'>
@@ -22,7 +45,23 @@ export default function SentencePopover(props: Props) {
                 </PopoverHeader>
                 <PopoverArrow />
                 <PopoverBody>
-                    <InputGroup size='sm'>
+                    {
+                        alternativeSentencesLoading ? <LoadingSpinner /> : <RadioGroup colorScheme="green" onChange={setSelectedSentence} value={selectedSentence}>
+                        <Stack direction='column'>
+                            {
+                                alternativeSentences.map(sentence => {
+                                    return <Radio value={sentence.id.toString()}>
+                                        <h3 className="text-left">
+                                            { sentence.Sentence }
+                                        </h3>
+                                    </Radio>
+                                })
+                            }
+                        </Stack>
+                      </RadioGroup>
+                    }
+
+                    <InputGroup className="mt-4" size='sm'>
                         <Input
                             placeholder='Your new sentence'
                             onChange={(event) => setNewSentence(event.target.value)}
@@ -30,7 +69,7 @@ export default function SentencePopover(props: Props) {
                         <InputRightElement>
                             <IconButton aria-label='Add your new sentence' className="ml-3" icon={<AddIcon />} />
                         </InputRightElement>
-                        </InputGroup>
+                    </InputGroup>
                 </PopoverBody>
             </PopoverContent>
     </Popover>
