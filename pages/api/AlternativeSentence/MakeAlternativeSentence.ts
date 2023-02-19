9dabf71a58bf5e328d5fd2b26905927696f6df25
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-02-19 20:09:29 +0000

    Added promise

--- pages/api/AlternativeSentence/MakeAlternativeSentence.ts
+++ pages/api/AlternativeSentence/MakeAlternativeSentence.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
 import { prisma } from "../../../lib/PrismaClient";
+import { AlternativeSentence } from "../../../lib/Types";
 
 type Data = {
-  	success?: boolean
+    sentence?: AlternativeSentence
+
+    success?: boolean
     error?: string
 }
 
@@ -12,22 +15,29 @@ export default async function handler(
 	res: NextApiResponse<Data>
 ) {
 
-    // Rejects the request if it doesn't have the new sentence or paragraph
-    if(!req.body.newSentence || !req.body.paragraph || !req.body.sentenceId) res.status(400).json({error: "Invalid request"})
-
-    await prisma.alternative_Sentences.create({
-        data: {
-            Alternative_To_Sentence: req.body.sentenceId,
-            Paragraph: req.body.paragraph,
-            Sentence: req.body.newSentence
+    new Promise<AlternativeSentence>(async (resolve, reject) => {
+        // Rejects the request if it doesn't have the new sentence or paragraph
+        if(!req.body.newSentence || !req.body.paragraph || !req.body.sentenceId) {
+            reject();
+            res.status(400).json({error: "Invalid request"})
         }
-    }).then(() => {        
-        res.status(200).json({
-            success: true
-        });
-    }).catch(error => {
-        res.status(200).json({
-            success: false
-        });
+
+        await prisma.alternative_Sentences.create({
+            data: {
+                Alternative_To_Sentence: req.body.sentenceId,
+                Paragraph: req.body.paragraph,
+                Sentence: req.body.newSentence
+            }
+        }).then((alternativeSentence) => {
+            resolve(alternativeSentence)
+            res.status(200).json({
+                sentence: alternativeSentence
+            });
+        }).catch(error => {
+            reject();
+            res.status(200).json({
+                success: false
+            });
+        })
     })
 }
