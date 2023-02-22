e8d60d7352239af0fa9d701ad074a1bcaced74fd
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-02-22 11:01:01 +0000

    Added /SetAlternativeSentence endpoint

---
+++ pages/api/AlternativeSentence/SetAlternativeSentence.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "../../../lib/PrismaClient";
+
+type Data = {
+  	success?: boolean
+    error?: string
+    sentences?: {}
+}
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+
+    // Rejects the request if it doesn't have the required parameters
+    if(!req.body.alternativeSentenceId || !req.body.sentenceId || !req.body.alternativeSentence || !req.body.sentence) res.status(400).json({error: "Invalid request"})
+
+    // Update the sentence to the alternative sentence
+    await prisma.sentences.update({
+        where: {
+            id: req.body.sentenceId
+        },
+        data: {
+            Sentence: req.body.alternativeSentence
+        }
+    });
+
+    // Update the alternative sentence to the sentence (swaps the alternative and the regular sentence)
+    await prisma.alternative_Sentences.update({
+        where: {
+            id: req.body.alternativeSentenceId
+        },
+        data: {
+            Sentence: req.body.sentence
+        }
+    });
+}
